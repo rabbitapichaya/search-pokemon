1fdee3167b3077f6ca0e641af44786d686f906a0
Apichaya <rabbitapichaya@gmail.com>
2025-02-23 01:48:13 +0700

    new images

--- components/PokemonDetails.tsx
+++ components/PokemonDetails.tsx
@@ -11,6 +11,7 @@ interface Evolution {
 }
 
 interface PokemonData {
+  id: number;
   name: string;
   types: string[];
   attacks: {
@@ -19,29 +20,58 @@ interface PokemonData {
   evolutions?: Evolution[];
 }
 
-const PokemonDetails = ({ data, router }: { data: PokemonData; router: any }) => {
+interface PokemonDetailsProps {
+  data: PokemonData;
+  router: any; // You can replace 'any' with a more specific type if available
+}
+
+const PokemonDetails: React.FC<PokemonDetailsProps> = ({ data, router }) => {
+  console.log("Pokemon Data:", data);
+
+const pokemonImage = data?.name
+  ? `https://img.pokemondb.net/artwork/large/${data.name.toLowerCase()}.jpg`
+  : "https://via.placeholder.com/200";
+  
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>{data.name}</h1>
+      <img className={styles.image} src={pokemonImage} alt={data.name} />
       <p className={styles.type}>Type: {data.types.join(', ')}</p>
+      
       <h2 className={styles.subtitle}>Special Attacks:</h2>
       <ul className={styles.list}>
         {data.attacks.special.map((attack) => (
-          <li key={attack.name} className={styles.listItem}>{attack.name} - {attack.damage} damage</li>
-        ))}
-      </ul>
-      <h2 className={styles.subtitle}>Evolutions:</h2>
-      <ul className={styles.list}>
-        {data.evolutions?.map((evolution) => (
-          <li key={evolution.name} className={styles.listItem}>
-            <a className={styles.link} onClick={() => router.push(`/pokemon/${evolution.name.toLowerCase()}`)}>
-              {evolution.name}
-            </a>
+          <li key={attack.name} className={styles.listItem}>
+            {attack.name} - {attack.damage} damage
           </li>
         ))}
       </ul>
+
+      <h2 className={styles.subtitle}>Evolutions:</h2>
+      <div className={styles.evolutionContainer}>
+        {data.evolutions?.map((evolution) => {
+          const evolutionImage = `https://img.pokemondb.net/artwork/large/${evolution.name.toLowerCase()}.jpg`;
+          return (
+            <div key={evolution.name} className={styles.evolutionItem}>
+              <img
+                className={styles.evolutionImage}
+                src={evolutionImage}
+                alt={evolution.name}
+              />
+              <a
+                className={styles.link}
+                onClick={() => router.push(`/pokemon/${evolution.name.toLowerCase()}`)}
+              >
+                {evolution.name}
+              </a>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
+  
 };
 
 export default React.memo(PokemonDetails);
